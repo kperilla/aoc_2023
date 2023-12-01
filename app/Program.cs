b325c044f7ec5414457ed59f0fc121193bf52357
Kevin Perilla <kevin.m.perilla@gmail.com>
2023-12-01 15:11:37 -0600

    Add runner code

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// using static AdventOfCodeRunner.Day1;
+
+// See https://aka.ms/new-console-template for more information
+namespace AdventOfCodeRunner
+{
+    public class AdventOfCodeRunner
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<(int, string), Func<int>> functionDictionary = new Dictionary<(int, string), Func<int>>
+            {
+                {(1, "a"), Day1.PartA},
+                {(1, "b"), Day1.PartB}
+            };
+            int dayNum = Convert.ToInt32(args[0]);
+            string part = args[1].ToLower();
+
+            Func<int> funcToRun = functionDictionary[(dayNum, part)];
+
+            Console.WriteLine(funcToRun());
+        }
+    }
+}
---
+++ app/day_1/Day1.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCodeRunner
+{
+    public class Day1
+    {
+        public static int PartA()
+        {
+            Console.WriteLine("Part A");
+            return 1;
+        }
+        public static int PartB()
+        {
+            Console.WriteLine("Part B");
+            return 1;
+        }
+    }
+}
