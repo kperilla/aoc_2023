cb88972e00e2420a9be16056e40561f5f7960314
Kevin Perilla <kevin.m.perilla@gmail.com>
2023-12-01 16:33:05 -0600

    Organize main

--- app/Program.cs
+++ app/Program.cs
@@ -1,26 +1,23 @@
-﻿// using static AdventOfCodeRunner.Day1;
-
-// See https://aka.ms/new-console-template for more information
-namespace AdventOfCodeRunner
+﻿namespace AdventOfCodeRunner
 {
     public class AdventOfCodeRunner
     {
-        static async Task Main(string[] args)
-        {
-            const string urlTemplate = "https://adventofcode.com/2023/day/{0}/input";
-            const string sessionPath = "./session.key";
-            Dictionary<(int, string), Func<string, string>> functionDictionary = new Dictionary<(int, string), Func<string, string>>
+        const string urlTemplate = "https://adventofcode.com/2023/day/{0}/input";
+        const string sessionPath = "./session.key";
+        static Dictionary<(int, string), Func<string, string>> functionDictionary = new Dictionary<(int, string), Func<string, string>>
             {
                 {(1, "a"), Day1.PartA},
                 {(1, "b"), Day1.PartB}
             };
+
+        static async Task Main(string[] args)
+        {
             int dayNum = Convert.ToInt32(args[0]);
             string part = args[1].ToLower();
             string url = String.Format(urlTemplate, dayNum);
             string sessionKey = SessionManager.ReadSessionFile(sessionPath);
-            Func<string, string> funcToRun = functionDictionary[(dayNum, part)];
-
             string inputString = await AocHttpClient.MakeGetRequest(url, sessionKey);
+            Func<string, string> funcToRun = functionDictionary[(dayNum, part)];
 
             Console.WriteLine(funcToRun(inputString));
         }
